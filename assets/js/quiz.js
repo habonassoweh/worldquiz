d870fc1a0e654ef25a7b32bc843617402947b4e0
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-03 14:20:55 -0400

    highscore html

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,24 @@
 var ul = document.getElementById("ul");
+var ulHigh = document.getElementById("ul-high");
 var nextButton = document.getElementById("btnNext");
 var quizbox = document.getElementById("questionBox");
 var opt1 = document.getElementById("opt1");
 var opt2 = document.getElementById("opt2");
 var opt3 = document.getElementById("opt3");
 var opt4 = document.getElementById("opt4");
+var timer = document.getElementById("timer");
+var gametime = 75;
+var timerId;
+timerId = setInterval(countDown, 1000);
 
+function countDown() {
+  gametime--;
+  timer.textContent = gametime;
+  if (gametime <= 0) {
+    window.alert("Time is Up.");
+    clearInterval(timerId);
+  }
+}
 var app = {
   questions: [
     {
@@ -16,7 +29,7 @@ var app = {
     {
       q: "What is the capital of Senegal",
       options: ["Touba", "Mogadishu", "Dakar", "Thies"],
-      answer: 2,
+      answer: 3,
     },
     {
       q: "What is the capital of Nepal",
@@ -34,4 +47,68 @@ var app = {
       answer: 1,
     },
   ],
+  index: 0,
+  //Load a question using the index
+  load: function () {
+    if (this.index <= this.questions.length - 1) {
+      quizbox.innerHTML = this.index + 1 + ". " + this.questions[this.index].q;
+      opt1.innerHTML = this.questions[this.index].options[0];
+      opt2.innerHTML = this.questions[this.index].options[1];
+      opt3.innerHTML = this.questions[this.index].options[2];
+      opt4.innerHTML = this.questions[this.index].options[3];
+    } else {
+      //Show the end screen
+      quizbox.innerHTML = "Quiz Completed!";
+      ul.style.display = "none";
+      nextButton.style.display = "none";
+    }
+  },
+  next: function () {
+    this.index++;
+    this.load();
+  },
+  //Check if answer is correct or not
+  check: function (ele) {
+    var id = ele.id.split("");
+    if (id[id.length - 1] == this.questions[this.index].answer) {
+      this.score++;
+      ele.className = "correct";
+      this.scoreCard();
+    } else {
+      ele.className = "wrong";
+      gametime = gametime - 5;
+    }
+  },
+  //disable options once user selects on option
+  preventClick: function () {
+    for (let i = 0; i < ul.children.length; i++) {
+      ul.children[i].style.pointerEvents = "none";
+    }
+  },
+  allowClick: function () {
+    for (let i = 0; i < ul.children.length; i++) {
+      ul.children[i].style.pointerEvents = "auto";
+      ul.children[i].className = "";
+    }
+  },
+  score: 0,
+  scoreCard: function () {
+    scoreCard.innerHTML = this.questions.length + "/" + this.score;
+  },
 };
+
+window.load = app.load();
+
+function button(ele) {
+  app.check(ele);
+  app.preventClick();
+}
+
+function next() {
+  app.next();
+  app.allowClick();
+}
+
+function endQuiz() {
+  clearInterval(timerId);
+}
